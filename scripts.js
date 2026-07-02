e88de9426cdabed5edb2c4ba57ad7f3a4906930e
Anna Beatriz <annabeatrizp999@gmail.com>
2026-07-02 20:07:02 -0300

    feito layout mobile e desktop do projeto mundo pet

--- scripts.js
+++ scripts.js
@@ -1,20 +1,128 @@
-const btnCloseModal = document.getElementById("close-modal")
-const btnOpenModal = document.getElementById("open-modal")
-const main = document.querySelector("main")
-const footer = document.querySelector("footer")
-const modal = document.getElementById("modal")
+/* selecionando itens */
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const morning = document.querySelector(".morning");
+const afternoon = document.querySelector(".afternoon");
+const night = document.querySelector(".night");
+/* modal */
+const btnOpenModal = document.getElementById("open-modal");
+const btnCloseModal = document.getElementById("close-modal");
+const modal = document.getElementById("modal");
+/* form */
+const form = document.querySelector("form");
+const inputTutor = document.getElementById("tutor-name");
+const inputPet = document.getElementById("pet-name");
+const textarea = document.getElementById("description");
+const inputDate = document.getElementById("date");
+const inputHour = document.getElementById("hour");
+
+const appointmentsList = [];
+
+// adicionando um novo atendimento
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // validando os inputs
+  if (inputTutor.value.length < 1) {
+    alert("Por favor, preencha o nome do tutor!");
+  } else if (inputPet.value.length < 1) {
+    alert("Por favor, preencha o nome do pet!");
+  } else if (textarea.value.length < 1) {
+    alert("Por favor, coloque a descrição do serviço!");
+  } else if (inputDate.value.length < 1) {
+    alert("Por favor, informe uma data para agendar!");
+  } else if (inputHour.value.length < 1) {
+    alert("Por favor, informe uma hora para agendar!");
+  }
+
+  // adicionando dados em um objeto
+  appointmentsList.push({
+    tutor: inputTutor.value,
+    pet: inputPet.value,
+    description: textarea.value,
+    date: inputDate.value,
+    hour: inputHour.value
+  })
+
+  // criando agendamento com os dados
+  const periodAppointments = document.createElement("div");
+  const appointment = document.createElement("div");
+  const div = document.createElement("div");
+  const time = document.createElement("p");
+  const petName = document.createElement("p");
+  const personName = document.createElement("span");
+  const span = document.createElement("span");
+  const description = document.createElement("span");
+  const a = document.createElement("a");
+
+  // adicionando classes e conteúdos
+  periodAppointments.classList.add("period-appointments");
+
+  appointment.classList.add("appointment");
+
+  time.classList.add("time");
+  time.textContent = inputHour.value;
+
+  petName.classList.add("pet-name");
+  petName.textContent = inputPet.value;
+
+  span.textContent = "/";
+
+  personName.classList.add("person-name");
+  personName.textContent = inputTutor.value;
+
+  description.classList.add("description");
+  description.textContent = textarea.value;
+
+  a.textContent = "Remover agendamento";
+
+  // inserindo na dom
+  div.append(time, petName, span, personName);
+  appointment.append(div, description, a);
+  periodAppointments.append(appointment);
+
+  // verificando horário
+  let hour = Number(inputHour.value.slice(0, 2));
+
+  if (hour <= 12) {
+    const noAppointments = document.querySelector(".morning .no-appointments")
+    
+
+    morning.append(periodAppointments);
+  } else if (hour <= 18) {
+    afternoon.append(periodAppointments);
+  } else if (hour <= 21) {
+    night.append(periodAppointments);
+  }
+
+  // fechando modal
+  closeModal();
+});
+
+// exibindo agendamentos na tela
 
 // abrindo modal
 btnOpenModal.addEventListener("click", () => {
-    main.classList.add("blur")
-    footer.classList.add("blur")
-    modal.classList.remove("hidden")
-})
+  openModal();
+});
 
 // fechando modal
 btnCloseModal.addEventListener("click", () => {
-    main.classList.remove("blur")
-    footer.classList.remove("blur")
-    modal.classList.add("hidden")
-})
+    closeModal();
+});
 
+// funções
+
+function closeModal() {
+    main.classList.remove("blur");
+    footer.classList.remove("blur");
+    modal.classList.add("hidden");
+}
+
+function openModal() {
+  main.classList.add("blur");
+  footer.classList.add("blur");
+  modal.classList.remove("hidden");
+}
+
+console.log(appointmentsList)
